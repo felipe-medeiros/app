71d25b70b8d44ef42e522c667f35d18db06e400e
Joel Tavares <jooeltavares@gmail.com>
2018-06-10 23:38:32 -0300

    Fix crud de carros

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,11 @@
 class Car < ApplicationRecord
   belongs_to :customer
+  validate :plate_valid
+  validates :plate, uniqueness: true, length: { minimum: 7 }
+
+  private
+
+    def plate_valid
+      self.plate = self.plate.strip.delete('-.').upcase
+    end
 end
